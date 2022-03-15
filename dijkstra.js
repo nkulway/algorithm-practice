5107329de773ee0956230211303887843bdc7e53
Nick Kulway <nkulway@gmail.com>
2022-03-14 21:35:08 -0400

    created helper method for larger function

--- dijkstra.js
+++ dijkstra.js
@@ -1,5 +1,5 @@
 
-
+// test graph to navigate
 let graph = {
   start: { A: 5, B: 2 },
   A: { start: 1, C: 4, D: 2 },
@@ -8,3 +8,26 @@ let graph = {
   D: { finish: 1 },
   finish: {}
 }
+
+let shortestDistanceNode = (distances, visited) => {
+  // create default value for shortest
+  let shortest = null
+
+  // for each node in the distance obj
+  for (let node in distances) {
+    // if no node has been assigned to shortest yet
+    // or if the current node's distance is smaller than
+    // the current shortest
+    let currentIsShortest = 
+        shortest === null || distances[node] < distances[shortest]
+
+    // and if the current node is in the unvisited set
+    if (currentIsShortest && !visited.includes(node)) {
+    // update shortest to be current node
+      shortest = node
+    }
+  }
+  return shortest
+}
+
+module.exports = shortestDistanceNode
